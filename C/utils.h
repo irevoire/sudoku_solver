a2a68b002823c0a10d079d66eeb46abaa413c9a9
irevoire <irevoire@hotmail.fr>
2018-06-21 16:38:06 +0200

    change the macro by inlined function

--- C/utils.h
+++ C/utils.h
@@ -5,11 +5,18 @@
 #define SIZE_OF_SUDOKU 9
 #define SIZE_OF_BLOCK 3
 
-#define HASH(x, y) \
-	((x) * 10 + (y))
+static inline int HASH(const int x, const int y)
+{
+	return x * 10 + y;
+}
 
-#define HASH_BLOC(x, y, n) \
-	( (((x) / 3 * 3) + (n / 3)) * 10 + (((y) / 3 * 3) + (n % 3)) )
+static inline int HASH_BLOC(const int x, const int y, const int n)
+{
+	const int xp = (x / 3 * 3 + n / 3) * 10;
+	const int yp = y / 3 * 3 + n % 3;
+
+	return xp + yp;
+}
 
 char *parse(FILE *input, char *grid);
 
