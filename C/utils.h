d502f8b9b847d7d9785390f5c15d6689044ceaaa
irevoire <irevoire@hotmail.fr>
2018-06-21 20:34:21 +0200

    test with assembly

--- C/utils.h
+++ C/utils.h
@@ -11,9 +11,19 @@ static inline int HASH(int x, int y)
 	return x * 10 + y;
 }
 
+static inline div_t my_div(int n)
+{
+	div_t div;
+	__asm__ ( "movl $0x0, %%edx;"
+			"movl %2, %%eax;"
+			"movl $0x3, %%ebx;"
+			"idivl %%ebx;" : "=a" (div.quot), "=d" (div.rem) : "g" (n) );
+	return div;
+}
+
 static inline int HASH_BLOC(int x, int y, int n)
 {
-	div_t ndiv = div(n, 3);
+	div_t ndiv = my_div(n);
 
 	const int xp = (x / 3 * 3 + ndiv.quot) * 10;
 	const int yp = y / 3 * 3 + ndiv.rem;
