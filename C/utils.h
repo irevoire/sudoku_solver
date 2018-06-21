d9d37928cf8897add7e34fe547c6161b43411f90
irevoire <irevoire@hotmail.fr>
2018-06-21 17:48:15 +0200

    try something with div function

--- C/utils.h
+++ C/utils.h
@@ -1,19 +1,22 @@
 #pragma once
 
 #include <stdio.h>
+#include <stdlib.h>
 
 #define SIZE_OF_SUDOKU 9
 #define SIZE_OF_BLOCK 3
 
-static inline int HASH(const int x, const int y)
+static inline int HASH(int x, int y)
 {
 	return x * 10 + y;
 }
 
-static inline int HASH_BLOC(const int x, const int y, const int n)
+static inline int HASH_BLOC(int x, int y, int n)
 {
-	const int xp = (x / 3 * 3 + n / 3) * 10;
-	const int yp = y / 3 * 3 + n % 3;
+	div_t ndiv = div(n, 3);
+
+	const int xp = (x / 3 * 3 + ndiv.quot) * 10;
+	const int yp = y / 3 * 3 + ndiv.rem;
 
 	return xp + yp;
 }
