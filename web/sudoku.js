d06d62eef9e78dca0aa908fb9af4097607c0fb0b
mlemesle <mlemesle@localhost.localdomain>
2018-07-21 21:08:14 +0200

    MartLem: sudoGrid parsing OK

--- web/sudoku.js
+++ web/sudoku.js
@@ -1,27 +1,40 @@
-function validateSudokuGrid() {
-    const regex = /^[1-9]$/;
-    let ret = true;
-    $('input').each(function() {
-        if (!regex.test($(this).val()))  {
-            $(this).addClass('error');
-            ret = false;
-        }
-        else $(this).removeClass('error');
+function validateAndGenerateSudokuGrid() {
+    const regex = /^[1-9]$|^$/;
+    let isValid = true;
+    let sudokuGrid = "";
+    $('tr').each(function() {
+        let tr = $(this);
+        tr.find("input").each(function() {
+            console.log($(this));
+            if (!regex.test($(this).val()))  {
+                $(this).addClass('error');
+                isValid = false;
+                sudokuGrid += '_';
+            }
+            else {
+                $(this).removeClass('error');
+                sudokuGrid += $(this).val() === '' ? '_' : $(this).val();
+            }
+            sudokuGrid += ',';
+        })
+        sudokuGrid = sudokuGrid.slice(0, -1) + '\n';
     });
 
-    return ret;
+    return isValid ? sudokuGrid : undefined;
 }
 
 function sendAjax() {
-    $.ajax('URL VERS LE SERVEUR', data : tableau, success : function() {
-
-    });
+    console.log("J'ai envoyé la grille !");
 }
 
 $(document).ready(function() {
-    $('#submit').click(function() {
-        if (validateSudokuGrid())
-            sendAjax();
-    })
-}
+        $('#submit').click(function() {
+            let sudokuGrid = validateAndGenerateSudokuGrid();
+
+            console.log(sudokuGrid);
+
+            if (undefined !== sudokuGrid)
+                sendAjax();
+        })
+    }
 );
