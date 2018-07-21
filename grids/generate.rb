14077fbfefadad5113bbb0656b8c3fcdaf660bee
irevoire <irevoire@hotmail.fr>
2018-07-21 17:19:09 +0200

    start some shit

--- grids/generate.rb
+++ grids/generate.rb
@@ -9,7 +9,9 @@ require_relative "../ruby/solver"
 puts "You'll can now enter your own sudoku grid ;"
 puts "<SPC> is used to set an unknow cell"
 puts "<1-9> to set a number"
-puts "<q> to exit"
+puts "<hjkl or arrow key or abcd> to move on the grid"
+puts "<ctrl + d> to say you finished entering the grid"
+puts "<q or ctrl + c> to exit"
 puts
 
 grid = Array.new(9) { Array.new(9) }
@@ -22,75 +24,70 @@ for i in 0..(9 * 4 + 2)
 end
 puts
 
+x = 0
 y = 0
-while y < 9
-	print "|"
-	x = 0
-	while x < 9
-		print "\e[1C"
+print "\e[2C"
+while true
+	input = STDIN.getch
 
-		input = STDIN.getch
+	# quit
+	exit if ["q", "\x03"].include?(input)
 
-		if input == "\n"
-			puts "blabla"
-		end
-
-		#         q    ctrl C
-		exit if ["q", "\u0003"].include?(input)
-
-		#   h   left  backspace
-		if ["h", "D", "\177"].include?(input) and x > 0
-			print "\e[4D"
-			print "\e[1D" if x % 3 ==  0
-			x -= 1
-		end
-
-		#   l   broken   tab
-		if ["l", "C", "\t", 0x4d].include?(input) and x < 8
-			x += 1
-			print "\e[4C"
-			print "\e[1C" if x % 3 ==  0
-		end
-
-		#   k    down
-		if ["k", "A"].include?(input) and y > 0
-			print "\e[1A"
-			print "\e[1A" if y % 3 ==  0
-			y -= 1
-		end
-
-		#   j    up
-		if ["j", "B"].include?(input) and y < 8
-			y += 1
-			print "\e[1B"
-			print "\e[1B" if y % 3 ==  0
-		end
-
-		if input == " "
-			print "\e[33;1mU\e[m"
-			grid[y][x] = nil
-		else
-			n = input.to_i
-
-			if n == 0 # mean to_i failed or user gave 0 which is not possible
-				print "\b"
-				redo
-			end
-			print "\e[32;1m#{n}\e[m"
-			grid[y][x] = n
-		end
-
-		print " |"
-		x += 1
-		print " " if x % 3 == 0
+	if [0x27, 0x91].include?(input)
+		redo
 	end
-	y += 1
+	# Finish
+	if ["f", "\x04"].include?(input)
+		break
 
-	puts
-	puts if y % 3 == 0
+	# left
+	elsif ["h", "D", "\177"].include?(input) and x > 0
+		print "\e[4D"
+		print "\e[1D" if x % 3 ==  0
+		x -= 1
+
+	# right
+	elsif ["l", "C", " ", "\t", 0x4d].include?(input) and x <= 9
+		x += 1
+		print "\e[6C"
+		print "\e[1C" if x % 3 ==  0
+
+	# down
+	elsif ["k", "A"].include?(input) and y > 0
+		print "\e[1A"
+		print "\e[1A" if y % 3 ==  0
+		y -= 1
+
+	# up
+	elsif ["j", "B"].include?(input) and y <= 9
+		y += 1
+		print "\e[1B"
+		print "\e[1B" if y % 3 ==  0
+
+	# either a number or nothing
+	else
+		n = input.to_i
+
+		if n == 0 # mean to_i failed or user gave 0 which is not possible
+			print "\e[2D"
+			redo
+		end
+		print "\e[32;1m#{n}\e[m"
+		print "\e[1C"
+		grid[y][x] = n
+		x += 1
+		print "\e[1C" if x % 3 ==  0
+	end
+
+	if x >= 9
+		x = 0
+		y += 1
+		puts if y % 3 == 0
+		puts "\e[2C"
+	end
 end
-puts
-puts
+
+(y..14).each { | a | puts }
 
 if !check(grid)
 	puts "\e[31;1mYour grid is not solvable\e[m\n"
